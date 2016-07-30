697fbaf3e7baa362842a345ac6907d77f7ff80f7
Doug S <hammer098@yahoo.com>
2016-07-30 19:24:26 -0400

    fix db

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20160730165213) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "members", force: :cascade do |t|
     t.string   "last_name"
     t.string   "first_name"
